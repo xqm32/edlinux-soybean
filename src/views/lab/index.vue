3c99cbe74cf3d8ffa20f149ac77dbdfd6565368a
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2024-04-18 13:04:11 +0800

    Implement lab

--- src/views/lab/index.vue
+++ src/views/lab/index.vue
@@ -18,5 +18,30 @@ function formatCode() {
 </script>
 
 <template>
-  <VueMonacoEditor v-model:value="code" theme="vs-dark" :options="MONACO_EDITOR_OPTIONS" @mount="handleMount" />
+  <NSplit direction="horizontal">
+    <template #1>
+      <NCard class="h-full" size="small" :bordered="false">
+        <NCode />
+      </NCard>
+    </template>
+    <template #2>
+      <NSplit direction="vertical" :default-size="0.8">
+        <template #1>
+          <NCard class="h-full" size="small" :bordered="false">
+            <VueMonacoEditor v-model:value="code" :options="MONACO_EDITOR_OPTIONS" @mount="handleMount" />
+          </NCard>
+        </template>
+        <template #2>
+          <NCard class="h-full" size="small" :bordered="false">
+            <NTabs animated>
+              <NTabPane name="测试用例"><NCode /></NTabPane>
+              <NTabPane name="测试结果"><NCode /></NTabPane>
+            </NTabs>
+          </NCard>
+        </template>
+      </NSplit>
+    </template>
+  </NSplit>
 </template>
+
+<style scoped></style>
