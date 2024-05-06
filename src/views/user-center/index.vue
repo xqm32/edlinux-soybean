4b95bcae0c5ff5b9dfcda4d5715e0be01e49bfa3
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2024-05-06 15:10:05 +0800

    Move fast

--- src/views/user-center/index.vue
+++ src/views/user-center/index.vue
@@ -1,24 +1,36 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onBeforeMount, ref } from 'vue';
 import { useEdLinux } from '@/hooks/common/edlinux';
 
 const { pb } = useEdLinux();
-const userModel = ref({
-  name: pb.authStore.model!.name,
-  number: pb.authStore.model!.number,
-  class: pb.authStore.model!.class
-});
+const getAvatarUrl = (model: any) => pb.getFileUrl(model, model.avatar, { download: true });
+
+const userModel = ref();
 async function updateUser() {
   await pb.collection('users').update(pb.authStore.model!.id, userModel.value);
   userModel.value = await pb.collection('users').getOne(pb.authStore.model!.id);
   window.$message!.success('修改成功');
 }
+async function updateUserAvatar({ file }: any) {
+  await pb.collection('users').update(pb.authStore.model!.id, { avatar: file.file });
+  userModel.value = await pb.collection('users').getOne(pb.authStore.model!.id);
+}
+
+onBeforeMount(async () => {
+  userModel.value = await pb.collection('users').getOne(pb.authStore.model!.id);
+});
 </script>
 
 <template>
-  <div>
+  <div v-if="userModel">
     <NCard>
       <NForm class="w-sm">
+        <NFormItem label="头像">
+          <img :src="getAvatarUrl(userModel)" class="h-32" />
+          <NUpload :show-file-list="false" :custom-request="updateUserAvatar">
+            <NButton class="ml-4">上传头像</NButton>
+          </NUpload>
+        </NFormItem>
         <NFormItem label="姓名">
           <NInput v-model:value="userModel.name" />
         </NFormItem>
