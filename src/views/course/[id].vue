ac98fedd7f1b8397901dca674e3a5444d10add22
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2024-05-06 10:39:18 +0800

    Move fast

--- src/views/course/[id].vue
+++ src/views/course/[id].vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 import { useActive, useEdLinux } from '@/hooks/common/edlinux';
 
 const { pb, isStudent, isTeacher } = useEdLinux();
@@ -11,6 +11,11 @@ const initChapters = async () => {
 };
 
 const course = ref();
+const courseModel = reactive({
+  name: '',
+  description: '',
+  teacherId: pb.authStore.model!.id
+});
 const joined = ref(false);
 const initCourse = async () => {
   const learn = (
@@ -20,10 +25,19 @@ const initCourse = async () => {
     })
   ).map(item => item.expand!.courseId.id);
   course.value = await pb.collection('courses').getOne(props.id);
+  courseModel.name = course.value.name;
+  courseModel.description = course.value.description;
   joined.value = learn.includes(props.id);
 };
 
 const [active, activate] = useActive();
+
+const [editActive, editActivate] = useActive();
+async function createCourse() {
+  await pb.collection('courses').update(props.id, courseModel);
+  initCourse();
+  editActive.value = false;
+}
 
 onMounted(async () => {
   Promise.all([initCourse(), initChapters()]);
@@ -38,7 +52,20 @@ onMounted(async () => {
           <template #header-extra>
             <NButton v-if="isStudent && !joined">加入课程</NButton>
             <NButton v-if="isStudent && joined" type="error">退出课程</NButton>
-            <NButton v-if="isTeacher">编辑课程</NButton>
+            <NButton v-if="isTeacher" @click="editActivate">编辑课程</NButton>
+            <NDrawer v-model:show="editActive" default-width="33%" resizable placement="right">
+              <NDrawerContent title="创建课程">
+                <NForm>
+                  <NFormItem label="课程标题">
+                    <NInput v-model:value="courseModel.name" />
+                  </NFormItem>
+                  <NFormItem label="课程描述">
+                    <NInput v-model:value="courseModel.description" type="textarea" :rows="16" />
+                  </NFormItem>
+                </NForm>
+                <NFlex justify="center"><NButton @click="createCourse">确定</NButton></NFlex>
+              </NDrawerContent>
+            </NDrawer>
           </template>
           <NCode :code="course.description" word-wrap />
         </NCard>
