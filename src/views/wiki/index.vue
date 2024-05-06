23c057619caa8a39150c97eba006fa1aa5f294bf
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2024-05-06 18:18:37 +0800

    Move fast

--- src/views/wiki/index.vue
+++ src/views/wiki/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { onBeforeMount, ref } from 'vue';
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
 import { useEdLinux } from '@/hooks/common/edlinux';
 
 const { pb } = useEdLinux();
@@ -11,20 +13,22 @@ async function initWikis() {
 onBeforeMount(async () => {
   await Promise.all([initWikis()]);
 });
+
+const quill = ref();
+function handleMount(element: any) {
+  quill.value = new Quill(element, { theme: 'snow' });
+}
 </script>
 
 <template>
   <div v-if="wikis">
-    <NGrid :cols="10">
-      <NGridItem :span="7">
-        <NCard title="知识库">
-          <div v-for="wiki in wikis" :key="wiki.id">
-            <NButton tag="a" :href="wiki.content" text>{{ wiki.name }}</NButton>
-          </div>
-        </NCard>
-      </NGridItem>
-      <NGridItem :span="3"><NCard title="管理"></NCard></NGridItem>
-    </NGrid>
+    <NCard title="知识库">
+      <template #header-extra><NButton>编辑</NButton></template>
+      <div v-for="wiki in wikis" :key="wiki.id">
+        <NButton tag="a" :href="wiki.content" text>{{ wiki.name }}</NButton>
+      </div>
+      <div :ref="handleMount"></div>
+    </NCard>
   </div>
 </template>
 
