158912dd9bf13d0aa2d2ea857d6b3c012327dcc4
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2024-05-06 11:11:58 +0800

    Move fast

--- src/views/chapter/[id].vue
+++ src/views/chapter/[id].vue
@@ -5,8 +5,10 @@ import 'vue-pdf-embed/dist/style/index.css';
 import 'vue-pdf-embed/dist/style/annotationLayer.css';
 import 'vue-pdf-embed/dist/style/textLayer.css';
 import { useEdLinux } from '@/hooks/common/edlinux';
+import { useRouterPush } from '@/hooks/common/router';
 
 const { pb, isTeacher } = useEdLinux();
+const { routerPush } = useRouterPush();
 const props = defineProps<{ id: string }>();
 const getFileUrl = (attachment: any) => pb.getFileUrl(attachment, attachment.content, { download: true });
 
@@ -29,6 +31,12 @@ const initChapter = async () => {
   chapter.value = await pb.collection('chapters').getOne(props.id);
 };
 
+async function deleteChapter() {
+  await pb.collection('chapters').delete(props.id);
+  window.$message!.success('删除成功');
+  await routerPush(`/course/${chapter.value.courseId}`);
+}
+
 onBeforeMount(async () => {
   await Promise.all([initChapter(), initAttachments(), initExercises()]);
 });
@@ -40,7 +48,10 @@ onBeforeMount(async () => {
       <NGridItem :span="7">
         <NCard :title="chapter.name">
           <template #header-extra>
-            <NButton v-if="isTeacher" type="error">删除章节</NButton>
+            <NPopconfirm @positive-click="deleteChapter">
+              <template #trigger><NButton type="error">删除章节</NButton></template>
+              确认删除章节？
+            </NPopconfirm>
           </template>
           <VuePdfEmbed
             v-if="chapter.content"
